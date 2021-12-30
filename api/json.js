b69bf61aff7666788281ca784732d3786fc9040c
chris48s <chris.shaw480@gmail.com>
2021-12-30 16:47:42 +0000

    bin react, convert to SSR app

--- api/json.js
+++ api/json.js
@@ -1,32 +1,27 @@
 const { unfurl } = require('unfurl.js');
 const url = require('url');
 
-const writeJsonResponse = function(response, code, body) {
+const writeJsonResponse = function (response, code, body) {
   response.writeHead(code, {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
+    'Access-Control-Allow-Origin': '*',
   });
   response.end(JSON.stringify(body));
 };
 
-module.exports = function(request, response) {
+module.exports = function (request, response) {
   const parsedUrl = url.parse(request.url, true);
-  if (['/api/', '/api'].indexOf(parsedUrl.pathname) == -1) {
-    writeJsonResponse(response, 404, {'error': 'not found'});
-  }
-
   if (request.method == 'GET') {
     const targetUrl = parsedUrl.query.url;
 
     unfurl(targetUrl)
-      .then(function(data) {
+      .then(function (data) {
         writeJsonResponse(response, 200, data);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         writeJsonResponse(response, 500, err);
       });
   } else {
-    writeJsonResponse(response, 405, {'error': 'method not allowed'});
+    writeJsonResponse(response, 405, { error: 'method not allowed' });
   }
-
 };
